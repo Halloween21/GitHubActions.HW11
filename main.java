9da060f173bb244a09ee853d0a9618ecedf3c32f
Halloween21 <simon@alacarte-kurier.de>
2022-01-11 17:41:30 +0100

    added main class with faculty method to test sonarcloud

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[] args) {
+        int a=5;
+        System.out.println(fak(5));
+    }
+    public static int fak( int a){
+        if (a==0 || a==1){
+            return 1;
+        }
+        else {
+            return fak(a-1)+fak(a-2);
+        }
+    }
+}

b1ac0c474de5ec655dfa046fab41521c7267f7db
Halloween21 <simon@alacarte-kurier.de>
2022-01-12 15:42:53 +0100

    formated code and added javadoc coment

--- main.java
+++ main.java
@@ -1,14 +1,20 @@
-public class main{
+public class main {
+
     public static void main(String[] args) {
-        int a=5;
+        int a = 5;
         System.out.println(fak(a));
     }
-    public static int fak( int a){
-        if (a==0 || a==1){
+
+    /**
+     * 
+     * @param a is the faculty we want to calculate
+     * @return the faculty number we want
+     */
+    public static int fak(int a) {
+        if (a == 0 || a == 1) {
             return 1;
-        }
-        else {
-            return fak(a-1)+fak(a-2);
+        } else {
+            return fak(a - 1) + fak(a - 2);
         }
     }
 }

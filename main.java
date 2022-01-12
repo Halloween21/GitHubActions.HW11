a953d3126dfbc9a1f2545326b39692beddb8ece1
Halloween21 <simon@alacarte-kurier.de>
2022-01-12 15:31:43 +0100

    fixed unused variable a

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main{
     public static void main(String[] args) {
         int a=5;
-        System.out.println(fak(5));
+        System.out.println(fak(a));
     }
     public static int fak( int a){
         if (a==0 || a==1){
